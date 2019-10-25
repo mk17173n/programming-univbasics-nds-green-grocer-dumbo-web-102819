a446cde86cdc79b94accec45763d64bfccffe31f
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 05:29:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,7 @@ def consolidate_cart(cart)
       item_hash = {}
       item_hash[cart[i][:item]] = {:price => cart[i][:price], :clearance => cart[i][:clearance]}
       item_hash[:count] = 1
-      newArr << object
+      newArr << item_hash
     else
       # newArr[cart[i][:count]] += 1
     end
